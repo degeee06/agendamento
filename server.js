fad1c92a156375125a5b72650437944d1872dd0b
degeee06 <cowla200@gmail.com>
2025-09-13 20:44:14 -0300

    Update server.js

--- server.js
+++ server.js
@@ -22,17 +22,6 @@ const planilhasClientes = {
 const clientesValidos = Object.keys(planilhasClientes);
 
 
-// Exemplo Node.js/Express
-const agendamentosExistentes = await db.query(
-  "SELECT * FROM agendamentos WHERE data = ? AND horario = ? AND status != 'cancelado'",
-  [data.Data, data.Horario]
-);
-
-if (agendamentosExistentes.length > 0) {
-  return res.status(400).json({ error: "Horário ocupado" });
-}
-
-
 
 // Google Service Account
 let creds;
@@ -275,6 +264,7 @@ app.get("/meus-agendamentos/:cliente", authMiddleware, async (req, res) => {
 });
 
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+
 
 
 
